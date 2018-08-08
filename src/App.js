29d64b6e4cc345bd5a11ceceaba7bef6888182b8
githubshivu <42203438+githubshivu@users.noreply.github.com>
2018-08-08 23:29:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          Hi 1
+          Hi 123
         </p>
       </div>
     );
